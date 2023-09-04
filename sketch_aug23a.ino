506948f7db2f73333006d28d20e1dff2234f33d5
Denis Nelkner <136069261+DenisTGD@users.noreply.github.com>
2023-09-04 10:51:07 +0200

    Update sketch_aug23a.ino
    Version V.1.0

--- sketch_aug23a.ino
+++ sketch_aug23a.ino
@@ -1,22 +1,25 @@
-
-  // put your setup code here, to run once:
-int on = 5V;
-int off = GND;
 int wet = A3;
 int dry = A3;
 
 
 void setup() {
       Serial.begin(9600);
+      pinMode(12 , OUTPUT);
   }
 
 
 void loop() {
+  
+   wet = analogRead(A3); 
+   dry = analogRead(A3);
+   
 
-    if (wet >= 80) {
-       Serial.println(off);
+    if (wet >= 350) {
+     digitalWrite(12 , LOW);
+     
   }
-    else if (dry <= 30){
-       Serial.println(on);
+    else if (dry <= 25){
+     digitalWrite(12 , HIGH);
+      
   }
 }
